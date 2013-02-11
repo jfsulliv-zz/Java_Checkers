14096934a557c192ddf2a928bc3ea06a30a0d828
James.Sullivan1 <calgaryjames@gmail.com>
2013-02-11 12:40:36 -0700

    Added src/Location.java, src/Piece.java

---
+++ src/Location.java
@@ -0,0 +1,42 @@
+/**
+ *  A Location Class. Holds X,Y Coordinates on the board.
+ *  @param xCoordinate the X-position
+ *  @param yCoordinate the Y-position
+ */
+
+public class Location {
+    private final int xCoordinate;
+    private final int yCoordinate;
+    
+    public Location(int x,int y){
+        if (x > 0 && y > 0){
+            this.xCoordinate = x;
+            this.yCoordinate = y;
+        } else {
+            System.out.print("Invalid coordinates");
+            this.xCoordinate = 1;
+            this.yCoordinate = 1;
+        }
+    }
+    
+    /**
+     *  Accessor method for X-position.
+     *  @return X-Coordinate
+     */
+    public int getX() {
+        return this.xCoordinate;
+    }
+    
+    /**
+     *  Accessor method for Y-position.
+     *  @return Y-Coordinate
+     */
+    public int getY() {
+        return this.yCoordinate;
+    }
+    
+    public String toString() {
+        return xCoordinate + "," + yCoordinate;
+    }
+    
+}
---
+++ src/Piece.java
@@ -0,0 +1,40 @@
+/**
+ *  A Checkers Piece. Holds information on piece colour, and King status.
+ *  <p> King status determines whether the piece can move bidirectionally.
+ *
+ *  @param king Boolean of king status
+ *  @param Colour enum of Colour (BLACK/RED)
+ */
+
+public class Piece {
+    public enum Colour {BLACK,RED};
+    
+    private boolean king = false;
+    private final Colour colour;
+    
+    public Piece(Colour aColour){
+        this.colour =  aColour;
+    }
+    
+    /**
+     *  Mutator method to King a piece.
+     *  <p> Post-Condition: Piece becomes a King.
+     */
+    public void makeKing() {
+        king = true;    
+    }
+    
+    /**
+     *  Accessor method to see King status.
+     *  @return true if the piece is a King.
+     */
+    public boolean isKing() {
+        return king;    
+    }
+    
+    public String toString() {
+        return colour.toString() + " is King: " + king;
+    }
+    
+}
+
