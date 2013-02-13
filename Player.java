759fa9a1cd3e616e44c1842129c444c3b0dc768b
zsanettrado <zsanett0rado@hotmail.com>
2013-02-13 14:14:58 -0700

    Added Player.java

---
+++ Player.java
@@ -0,0 +1,24 @@
+/**
+ * This class represents our player which owns the pieces, can win, can loose,
+ * and can quit.
+ * 
+ * @author Zsanett
+ * 
+ */
+public class Player {
+
+	/**
+	 * Each player owns 12 pieces.
+	 */
+
+	public Player(int colour) {
+		if (colour == Piece.RED){
+			Piece[] pieces = new Piece[12];
+		}
+		else if (colour == Piece.BLACK){
+			Piece[] pieces = new Piece[12];
+		}
+	}
+}
+
+	
