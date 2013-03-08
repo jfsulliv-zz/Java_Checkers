3dcb27e0f1bfb74d51a57b754ce15fc74ac38236
James.Sullivan1 <calgaryjames@gmail.com>
2013-03-08 11:03:23 -0700

    Updated readme, small changes to Board, modified Test cases

--- README.md
+++ README.md
@@ -4,25 +4,27 @@ Group 13 Project - CPSC 219
 
 Team Members:
 
-    Dylan Dobbyn
-    Daniel Contreras
-    Zsanette Rado
-    James Sullivan (Lead)
+    <ul>
+        <li>Dylan Dobbyn</li>
+        <li>Daniel Contreras</li>
+        <li>Zsanette Rado</li>
+        <li>James Sullivan (Lead)</li>
+    </ul>
 
 -----
 PROJECT INFORMATION
 -----
 
   The project will consist of a simple Checkers game written in Java.
-  
+  <p>
   Features:
-  
-    -Graphical interface including menus, board, etc
-    -Two-player checkers game
-    -One-player checkers game vs. simple AI
-    -Scoreboard(?)
-    -Tournament mode(?)
-
+    <ul>
+        <li>Graphical interface including menus, board, etc</li>
+        <li>Two-player checkers game</li>
+        <li>One-player checkers game vs. simple AI</li>
+        <li>Scoreboard(?)</li>
+        <li>Tournament mode(?)</li>
+    </ul>
   
   
 -----
@@ -30,11 +32,11 @@ DOCUMENTATION
 -----
 
   All documentation will be recorded in the Code Repository (GitHub), in .txt format
-  
+  <p>
   Each deliverable will contain one summary Document which contains information on the work that was done
   during the deliverable, and updates on the organization of the project.
-  
-  These Documents will be commited to the Documentation branch.
+  <p>
+  These Documents will be commited to the Documentation Directory.
 
 -----
 CODE FORMAT
@@ -42,12 +44,12 @@ CODE FORMAT
 
   Code will be written with in an object-oriented fashion. All classes will have relevant documentation as a header,
   including all relevant methods.
-  
+  <p>
   4-space Indentation will be used for consistency.
-  
+  <p>
   Documentation on all classes will be provided giving basic insight into their purpose and their contained methods.
   In addition, line documentation will be used in any relevant areas for clarity.
-  
+  <p>
   The Code will be commited to the Project branch.
   
 -----
@@ -55,12 +57,16 @@ HOW-TO: GIT
 -----
 
 Pulling<p>
-    1. Create and sync a local folder with the Git repository (git clone https://github.com/JamesSullivan1/CPSC219_Team13_Project.git)<p>
-    2. Pull the most recent version of the repository (git pull) <p>
+    <ol>
+        <li>Create and sync a local folder with the Git repository (git clone https://github.com/JamesSullivan1/CPSC219_Team13_Project.git)</li>
+        <li>Pull the most recent version of the repository (git pull)</li>
+    </ol>
 
-Pushing<p>
-    1. Pull the most recent version of the repository (git pull)<p>
-    2. Make any changes to files, add new files, et cetera<p>
-    3. Enter the command (git add -A) to add all updated files to the local repository<p>
-    4. Enter the command (git commit -m "YOUR MESSAGE HERE") to give the commit a brief description<p>
-    5. Enter the command (git push origin master) to push the local version to the global repository<p>
+Pushing
+    <ol>
+        <li>Pull the most recent version of the repository (git pull)</li>
+        <li>Make any changes to files, add new files, et cetera</li>
+        <li>Enter the command (git add -A) to add all updated files to the local repository</li>
+        <li>Enter the command (git commit -m "YOUR MESSAGE HERE") to give the commit a brief description</li>
+        <li>Enter the command (git push origin master) to push the local version to the global repository</li>
+    </ol>
--- src/test/TestBlackPlayer.java
+++ src/test/TestBlackPlayer.java
@@ -1,18 +1,30 @@
 package test;
 
-import main.Board;
-import main.HumanPlayer;
+import main.*;
 
+/**
+ * Tester class to perform a series of test cases on the Black Player's movements.
+ * Tests a wide variety of posible legal and illegal movements.
+ *
+ */
 public class TestBlackPlayer {
 
 	private String errors = " ";
 	private Board board = new Board();
-	private HumanPlayer player1, player2;
+	private HumanPlayer playerRed = new HumanPlayer(Colour.RED, board);
+	private HumanPlayer playerBlack = new HumanPlayer(Colour.BLACK, board);
+	private static boolean SILENT = true;
+	
+	private void setup() {
+		board.resetBoard();
+	}
 
 	private boolean black_moves_too_far_down() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location start = new Location(0,2);
+		Location end = new Location(2,3);
+		if(board.checkMove(playerBlack, start, end, false, SILENT)) {
 			testPassed = false;
 			errors += String
 					.format("black_moves_too_far_down failed: Black should not"
@@ -22,9 +34,11 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_moves_to_a_space_occupied_by_black() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location start = new Location(1,1);
+		Location end = new Location(2,2);
+		if(board.checkMove(playerBlack, start, end, false, SILENT)){
 			testPassed = false;
 			errors += String
 					.format("black_moves_to_a_space_occupied_by_black failed: "
@@ -34,9 +48,15 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_moves_to_a_space_occupied_by_red() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location blackStart = new Location(2,2);
+		Location redStart = new Location(1,5);
+		Location redMid = new Location(0,4);
+		Location end = new Location(1,3);
+		playerRed.movePiece(redStart,redMid);
+		playerRed.movePiece(redMid,end);
+		if(board.checkMove(playerBlack, blackStart, end, false, SILENT)) {
 			testPassed = false;
 			errors += String
 					.format("black_moves_to_a_space_occupied_by_red failed: Black"
@@ -46,9 +66,12 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_moves_up() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location start = new Location(2,2);
+		Location end = new Location(3,3);
+		playerBlack.movePiece(start,end);
+		if(board.checkMove(playerBlack, end, start , false, SILENT)) {
 			testPassed = false;
 			errors += String.format("black_moves_up failed: Black should not"
 					+ " be able to move up. \n");
@@ -57,9 +80,11 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_moves_down() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location start = new Location(6,2);
+		Location end = new Location(5,3);
+		if(!board.checkMove(playerBlack, start, end, false, SILENT)) {
 			testPassed = false;
 			errors += String.format("black_moves_down failed: Black should be"
 					+ " able to move down. \n");
@@ -68,9 +93,15 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_jumps_a_red_piece_to_an_empty_space() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location blackStart = new Location(2,2);
+		Location blackEnd = new Location(3,3);
+		Location redStart = new Location(1,5);
+		Location redEnd = new Location(2,4);
+		playerBlack.movePiece(blackStart,blackEnd);
+		playerRed.movePiece(redStart,redEnd);
+		if(!board.checkMove(playerBlack, blackEnd, redStart, true, SILENT)){
 			testPassed = false;
 			errors += String
 					.format("black_jumps_a_red_piece_to_an_empty_space failed: "
@@ -81,9 +112,16 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_jumps_a_red_piece_to_an_occupied_space() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location blackStart = new Location(2,2);
+		Location blackMid = new Location(1,3);
+		Location blackEnd = new Location(3,5);
+		Location red1Start = new Location(1,5);
+		Location red1End = new Location(2,4);
+		playerRed.movePiece(red1Start,red1End);
+		playerBlack.movePiece(blackStart,blackMid);
+		if(board.checkMove(playerBlack, blackMid, blackEnd, true, SILENT)) {
 			testPassed = false;
 			errors += String
 					.format("black_jumps_a_red_piece_to_an_occupied_space failed: "
@@ -94,9 +132,11 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_jumps_a_friendly_piece() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location start = new Location(1,1);
+		Location end = new Location(3,3);
+		if(board.checkMove(playerBlack, start, end, true, SILENT)) {
 			testPassed = false;
 			errors += String
 					.format("black_jumps_a_friendly_piece failed: Black should "
@@ -105,22 +145,16 @@ public class TestBlackPlayer {
 		return testPassed;
 	}
 
-	private boolean black_jumps_an_empty_space() {
-		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
-			testPassed = false;
-			errors += String
-					.format("black_jumps_an_empty_space failed: Black should not be able"
-							+ " to jump \"nothing\". \n");
-		}
-		return testPassed;
-	}
-
 	private boolean black_jumps_too_far() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location blackStart = new Location(4,2);
+		Location redStart = new Location(1,5);
+		Location redMid = new Location(2,4);
+		Location redEnd = new Location(3,3);
+		playerRed.movePiece(redStart,redMid);
+		playerRed.movePiece(redMid,redEnd);
+		if(board.checkMove(playerBlack, blackStart, redStart, true, SILENT)) {
 			testPassed = false;
 			errors += String.format("black_jumps_too_far failed: Black"
 					+ " should not be able to jump greater than 2 rows. \n");
@@ -129,9 +163,11 @@ public class TestBlackPlayer {
 	}
 
 	private boolean black_attempts_moving_black_piece() {
+		setup();
 		boolean testPassed = true;
-		board.initializeBoard();
-		if (testPassed) {
+		Location redStart = new Location(1,5);
+		Location redEnd = new Location(2,4);
+		if(board.checkMove(playerBlack, redStart, redEnd, true, SILENT)) {
 			testPassed = false;
 			errors += String
 					.format("black_attempts_moving_black_piece failed: "
@@ -140,7 +176,7 @@ public class TestBlackPlayer {
 		return testPassed;
 	}
 
-	public static void main(String[] args) {
+	public void run() {
 		boolean testsPassed = true;
 		TestBlackPlayer test = new TestBlackPlayer();
 		
@@ -152,14 +188,13 @@ public class TestBlackPlayer {
 		testsPassed &= test.black_jumps_a_red_piece_to_an_empty_space();
 		testsPassed &= test.black_jumps_a_red_piece_to_an_occupied_space();
 		testsPassed &= test.black_jumps_a_friendly_piece();
-		testsPassed &= test.black_jumps_an_empty_space();
 		testsPassed &= test.black_jumps_too_far();
 		testsPassed &= test.black_attempts_moving_black_piece();
 		
 		if (testsPassed) {
-			System.out.println("All tests passed");
+			System.out.println("All tests on Black pieces passed\n\n");
 		} else {
-			System.out.println(test.errors);
+			System.out.println(test.errors + "\n\n");
 		}
 	}
 
