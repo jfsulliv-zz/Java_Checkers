90cfc2f223f2fdd3d3d991424c378d79abe351e9
DylanDobbyn <dwdobbyn@ucalgary.ca>
2013-03-20 15:14:33 -0600

    Test for panel

---
+++ mainFrame/panelTest/GamePanelTest.java
@@ -0,0 +1,17 @@
+package panelTest;
+import GUI.*;
+import javax.swing.JFrame;
+
+public class GamePanelTest{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GamePanel panel1 = new GamePanel();
+        frame.add(panel1);
+        frame.setSize(925, 600);
+        frame.setVisible(true);
+        }
+    }
+
+
+
