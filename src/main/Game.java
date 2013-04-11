3eb476676b03c6e2be1da867a51c0e6a9ac74693
James.Sullivan1 <calgaryjames@gmail.com>
2013-04-11 15:08:02 -0600

    fix

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,6 +1,7 @@
 package main;
 
 import userInterface.controller.ScoreDataHandler;
+import javax.swing.SwingUtilities;
 
 public class Game {
 	private static Game instance;
@@ -73,8 +74,15 @@ public class Game {
 			
 			// The following loop will only end when the Observable Board's state has changed, ie a piece has been moved.
 			while(currentPlayer.isMyTurn() == true) {
+				SwingUtilities.isEventDispatchThread();
 				if(!currentPlayer.isHuman()){
 					currentPlayer.makeCurrentMove();
+				}
+				
+				try{
+					Thread.sleep(30);
+				}catch (InterruptedException ie){
+					ie.printStackTrace();
 				}
 			}
 		}
